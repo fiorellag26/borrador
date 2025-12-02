12ceba09d37cdf7b1b25c6046ec05efd56617532
400305_Galetto <400305@sistemas.frc.utn.edu.ar>
2025-12-02 15:45:24 -0300

    ms-auth-test funciona localmente con mvn spring-boot:run y keycloak si levanta el docker

--- ms-auth-test/src/main/java/com/tpi/ms_auth_test/MsAuthTestApplication.java
+++ ms-auth-test/src/main/java/com/tpi/ms_auth_test/MsAuthTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsAuthTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsAuthTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuthTestApplication.class, args);
+    }
 
 }
